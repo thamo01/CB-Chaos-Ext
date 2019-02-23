350a0c2e5ef66511d1f59a45cdccd9d8ef176578
Agash Thamotharampillai <agashthamo@Agashs-MacBook-Pro.local>
2019-02-23 10:42:28 +0100

    initial boilerplate for chrome extension

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    devtool: "inline-source-map",
+
+    entry: {
+        content: './src/content/index.ts',
+        background: './src/background/index.ts',
+        popup: './src/popup/index.tsx',
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: '[name].js'
+    },
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+
+    module: {
+        rules: [{
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+};
